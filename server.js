acb54f0fb97fd951b18cc5792c85e2ecc2fd18f7
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-04-08 13:50:46 +0200

    Add cors

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ const port = 3002;
 
 const graphRouter = require('./router/graph');
 
+const corsOptions = {
+    origin: 'http://10.92.0.113:5173',  // Allow requests from the frontend (which is running on this IP and port)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
 
-app.use(cors());
+
+app.use(cors(corsOptions));
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
