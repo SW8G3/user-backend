ddbe8c8e41ffb349639354c333035f78534e43e5
elisabetasta <eolafs24@student.aau.dk>
2025-04-09 12:42:31 +0200

    Add HTTPS support and update README for local setup

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+
 const app = express();
 const port = 3002;
 
-
 const graphRouter = require('./router/graph');
 
+// SSL certificate and key from mkcert
+const key = fs.readFileSync(path.join(__dirname, 'localhost+2-key.pem'));
+const cert = fs.readFileSync(path.join(__dirname, 'localhost+2.pem'));
 
+// Middleware
 app.use(cors());
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// Routes
 app.use('/graph', graphRouter);
 
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// Start HTTPS server
+https.createServer({ key, cert }, app).listen(port, () => {
+  console.log(`🚀 HTTPS server running at https://localhost:${port}`);
 });
