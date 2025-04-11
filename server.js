9d44dd634f8a7d3c8a3a86d41480c8c90857ff4f
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-04-11 11:00:49 +0200

    Remove HTTPS

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const https = require('https');
-const path = require('path');
 
 const app = express();
 const port = 3002;
@@ -10,11 +7,6 @@ const port = 3002;
 const graphRouter = require('./router/graph');
 
 
-// SSL certificate and key from mkcert
-const key = fs.readFileSync(path.join(__dirname, '10.92.0.113+3-key.pem'));
-const cert = fs.readFileSync(path.join(__dirname, '10.92.0.113+3.pem'));
-
-/*
 const corsOptions = {
   origin: 'https://10.92.0.113:5173', // Ensure this matches your frontend's HTTPS URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -23,15 +15,7 @@ const corsOptions = {
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
-*/
 
-const corsOptions = {
-  origin: '*', // Allow all origins (for debugging only)
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
 
 // Middleware
 app.use(cors(corsOptions));
@@ -42,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/graph', graphRouter);
 
-// Start HTTPS server
-https.createServer({ key, cert }, app).listen(port, '0.0.0.0', () => {
-  console.log(`🚀 HTTPS server running at https://localhost:${port}`);
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
